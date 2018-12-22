cf612824aebdc4aac6cc1007d382d4ee9c46710a
keenwon <semanwmj@gmail.com>
2018-12-22 20:08:51 +0800

    eslint 添加 env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
-'use strict';
+"use strict"
 
 module.exports = {
-  'extends': [
-    'xo/esnext',
-    'xo/browser'
-  ]
-};
+  extends: ["xo/esnext", "xo/browser"],
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es6: true,
+    mocha: true,
+    jest: true
+  }
+}
